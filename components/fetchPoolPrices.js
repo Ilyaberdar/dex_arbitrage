13ff5e484ac7f2c7749905b0ae8b3ccf5b3ffd0a
Gamzatel <user2014.uu33@gmail.com>
2025-04-26 14:45:23 +0300

    add V2 pool prices

--- components/fetchPoolPrices.js
+++ components/fetchPoolPrices.js
@@ -1,5 +1,5 @@
 // testPriceFetch.js
-const { getPriceFromV3Pool } = require('./dexPriceFetcher.js');
+const { getPriceFromV3Pool, getPriceFromV2Pool } = require('./dexPriceFetcher.js');
 const { TOKEN } = require('../config/tokens.js');
 const { POOL } = require('../config/liquidityPool.js');
 const { logger } = require('../utils/log.js');
@@ -24,8 +24,11 @@ const getPoolPrices = async () => {
   //const currentUniswapPoolPrice = await getPriceFromV3Pool(POOL.UNISWAP_WBTC, TOKEN.WBTC, TOKEN.USDC /*,1*/); //@note: amountIn param to pass in the equivalent of a token in the pool 
   //const currentSushiSwapPoolPrice = await getPriceFromV3Pool(POOL.SUSHISWAP_WBTC, TOKEN.WBTC, TOKEN.USDC);
 
-  const currentUniswapPoolPrice = await getPriceFromV3Pool(POOL.UNISWAP_WETH, TOKEN.WETH, TOKEN.USDC, 300, 0.03);
-  printRouterPrice('Uniswap V3', currentUniswapPoolPrice);
+  //const currentUniswapPoolPrice = await getPriceFromV3Pool(POOL.UNISWAP_WETH, TOKEN.WETH, TOKEN.USDC, 300, 0.03);
+  //printRouterPrice('Uniswap V3', currentUniswapPoolPrice);
+
+  const currentUniswapPoolPriceV2 = await getPriceFromV2Pool(POOL.UNISWAP_ETH_V2, TOKEN.WETH, TOKEN.USDC, 300, 0.03);
+  printRouterPrice('Uniswap V2', currentUniswapPoolPriceV2);
 
   //const currentSushiSwapPoolPrice = await getPriceFromV3Pool(POOL.SUSHISWAP_WETH, TOKEN.WETH, TOKEN.USDC, 1, 0.03);
   //printRouterPrice('SushiSwap V3', currentSushiSwapPoolPrice);
---
+++ config/uniswapV2PoolABI.js
@@ -0,0 +1,52 @@
+const UNISWAP_V2_POOL_ABI = [
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getReserves",
+      "outputs": [
+        { "name": "_reserve0", "type": "uint112" },
+        { "name": "_reserve1", "type": "uint112" },
+        { "name": "_blockTimestampLast", "type": "uint32" }
+      ],
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "token0",
+      "outputs": [
+        { "name": "", "type": "address" }
+      ],
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "token1",
+      "outputs": [
+        { "name": "", "type": "address" }
+      ],
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+        { "name": "", "type": "uint256" }
+      ],
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "factory",
+      "outputs": [
+        { "name": "", "type": "address" }
+      ],
+      "type": "function"
+    }
+  ];
+  
+  module.exports = { UNISWAP_V2_POOL_ABI };
+  
